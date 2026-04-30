3250fccae305e53c6f4349eb4f9506008e1a67dd
KoJuju <kojuju@foxmail.com>
2026-04-30 16:55:18 +0800

    chore: initialize project from platform template

---
+++ src/main/resources/db/data-h2.sql
@@ -0,0 +1,4 @@
+INSERT INTO sys_user_demo (user_name, email, status, created_at, updated_at)
+VALUES
+    ('admin', 'admin@example.com', 'ACTIVE', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('demo-user', 'demo.user@example.com', 'ACTIVE', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
