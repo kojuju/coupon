8228ab2a494872b589cac07af9df089387bc0d1e
KoJuju <kojuju@foxmail.com>
2026-04-30 16:55:20 +0800

    chore: initialize project from platform template

---
+++ src/test/java/com/kojuju/coupon/SpringMybatisPlusTemplateApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kojuju.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMybatisPlusTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
